341026a1c78d279cd0e4b10e8ef1b8181737bc8e
David <h1017733@gmail.com>
2019-11-05 23:18:42 +0800

    remove complier js files

--- src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var lib_1 = require("./lib");
-(function () {
-    lib_1.default.showMessage('HelloWorld!!', '#root');
-})();
